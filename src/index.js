dfbfdfd585e527efe016bc6dcdd1e854aef8f79a
akym19 <raffyjay_dayag@yahoo.com>
2024-02-06 14:46:09 +0800

    started JS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { generateHomepage } from "./js/homepage";
 import "./css/styles.css";
+import projects from "./js/sampleDB.json";
 
 generateHomepage();
 
-console.log("Test");
+console.log(projects);
